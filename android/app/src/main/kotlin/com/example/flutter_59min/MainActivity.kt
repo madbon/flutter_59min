959f1fee993adeb69c8caa59208a80891484b049
Mark Angelo Bon <mdbon@dilg.gov.ph>
2022-10-28 15:56:54 +0800

    push project

---
+++ android/app/src/main/kotlin/com/example/flutter_59min/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_59min
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/learn_flutter_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class LearnFlutterPage extends StatefulWidget {
+  const LearnFlutterPage({super.key});
+
+  @override
+  State<LearnFlutterPage> createState() => _LearnFlutterPageState();
+}
+
+class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Learn Flutter'),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop(); // delete the page
+            },
+            icon: const Icon(Icons.arrow_back_ios)),
+      ),
+      body: Column(
+        children: [
+          Image.asset('images/selfie.jpg'),
+          const SizedBox(
+            height: 10,
+          ),
+          const Divider(
+            color: Colors.black,
+          ),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
+            color: Colors.blueGrey,
+            width: double.infinity,
+            child: const Center(
+              child: Text(
+                'This is a text widget',
+                style: TextStyle(color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                debugPrint('Elevated button');
+              },
+              child: const Text('Elevated button'))
+        ],
+      ),
+    );
+  }
+}
